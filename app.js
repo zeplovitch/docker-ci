51424fac1b54e244c34084c8f658792f86759efa
Dror <drorz@hubx.com>
2018-12-27 16:08:57 -0500

    circle ci

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express();
+// Sample node.js web app for Pluralsight Docker CI course
+// For demonstration purposes only
+'use strict';
 
-app.get('/', (req, res) => {
-  res.send('Hello World! CI')
+var express = require('express'),
+    app = express();
+
+app.set('views', 'views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+    res.render('home', {
+  });
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
-});
+app.listen(8080);
 module.exports.getApp = app;
