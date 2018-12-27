b8b038b5eb751d4df3f285d5d31e3ddc008b8c6c
Dror <drorz@hubx.com>
2018-12-27 17:32:34 -0500

    ni

--- app.js
+++ app.js
@@ -5,12 +5,10 @@
 var express = require('express'),
     app = express();
 
-app.set('views', 'views');
-app.set('view engine', 'jade');
+
 
 app.get('/', function(req, res) {
-    res.render('home', {
-  });
+  res.send("hello");
 });
 
 app.listen(8080);
